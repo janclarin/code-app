5a84d22d63d116ac4a232578bb1429f1faaf8723
Jan Clarin <janclarin@outlook.com>
2015-02-20 21:03:02 -0700

    add button for careerpathdetail

--- app/src/main/java/com/wendiesel/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/wendiesel/myapplication/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.wendiesel.myapplication.activity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.wendiesel.myapplication.R;
 
@@ -14,6 +17,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.btn_example_path_detail);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CareerPathDetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
