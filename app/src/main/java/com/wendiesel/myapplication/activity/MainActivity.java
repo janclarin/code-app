8a06001303b697d4c089452d0f8a5dd050df3705
Jan Clarin <janclarin@outlook.com>
2015-02-20 22:34:37 -0700

    change theme and add viewpager

--- app/src/main/java/com/wendiesel/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/wendiesel/myapplication/activity/MainActivity.java
@@ -1,23 +1,57 @@
 package com.wendiesel.myapplication.activity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.wendiesel.myapplication.R;
+import com.wendiesel.myapplication.fragment.EmploymentInfoFragment;
+import com.wendiesel.myapplication.fragment.ListCareerPathFragment;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+        implements ListCareerPathFragment.OnListCareerPathListener,
+        EmploymentInfoFragment.OnEmploymentInfoListener {
+
+    private Toolbar mToolbar;
+    private CustomPagerAdapter mAdapter;
+    private PagerSlidingTabStrip mTabStrip;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        // Find views.
+        mTabStrip = (PagerSlidingTabStrip) findViewById(R.id.tab_strip);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
         Button button = (Button) findViewById(R.id.btn_example_path_detail);
+
+        // Set view pager adapter.
+        mAdapter = new CustomPagerAdapter(getFragmentManager());
+        mViewPager.setAdapter(mAdapter);
+
+        // Set page margin.
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        mViewPager.setPageMargin(pageMargin);
+
+        mTabStrip.setViewPager(mViewPager);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,5 +82,35 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class CustomPagerAdapter extends FragmentPagerAdapter {
+        private final String[] TITLES = {"Career Paths", "Employment Info"};
+
+        public CustomPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return ListCareerPathFragment.newInstance();
+                case 1:
+                    return EmploymentInfoFragment.newInstance();
+                default:
+                    return ListCareerPathFragment.newInstance();
+            }
+        }
     }
 }
