95b8804f76b56465a9c0d68782144ec04930559c
Jan Clarin <janclarin@outlook.com>
2015-02-22 08:12:24 -0700

    sorting begins...

--- app/src/main/java/com/wendiesel/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/wendiesel/myapplication/activity/MainActivity.java
@@ -85,18 +85,15 @@ public class MainActivity extends ActionBarActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_account) {
-            Intent intent = new Intent(MainActivity.this, YourInformationActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
+        switch (id) {
+            case R.id.action_account:
+                Intent intent = new Intent(MainActivity.this, YourInformationActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
