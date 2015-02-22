3e815efc97d662e3def133ba07b6ecea5459d77a
Atheng <andrewheng2@gmail.com>
2015-02-22 03:04:39 -0700

    changed edu info string

--- app/src/main/java/com/wendiesel/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/wendiesel/myapplication/activity/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends ActionBarActivity
     }
 
     public class CustomPagerAdapter extends FragmentPagerAdapter {
-        private final String[] TITLES = {"Employment Information", "Fields of Interest"};
+        private final String[] TITLES = {"Employment Info", "Fields of Interest"};
 
         public CustomPagerAdapter(FragmentManager fragmentManager) {
             super(fragmentManager);
